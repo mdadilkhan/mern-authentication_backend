b622cc9a8005ae48bcfde8556dfb03711f3e49bc
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2023-12-31 19:53:23 +0530

    added sigin functionality

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+import  jwt  from 'jsonwebtoken';
 import { errorHandler } from '../utils/error.js';
 
 
@@ -33,7 +34,34 @@ export const signup=async(req,res,next)=>{
 
 } 
 
+function generateJwtToken(payload, secretKey) {
+  const token = jwt.sign(payload, secretKey);
+  return token;
+}
 
-export const signin=(req,res,next)=>{
-  
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    console.log('isnide signin');
+    try {
+    console.log('try');
+
+      const validUser=await User.findOne({email});
+      if(!validUser)return next(errorHandler(404,'User Not Found'))
+      const validPassword=await bcrypt.compare(password,validUser.password)
+      if(!validPassword)return next(errorHandler(401,'Invalid Credential'))
+      const token = generateJwtToken({ id: validUser._id },process.env.JWT_SECRET)
+      const expiryDate = new Date(Date.now() + 3600000);
+      const {password:hashedPassword,...rest}=validUser._doc
+      res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200) 
+      .json(rest);
+    } catch (error) {
+      console.log('catch');
+      next(error)
+    }
+
+
+
+    
 }
